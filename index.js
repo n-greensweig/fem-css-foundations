04b68f486ee57cd957e8c6a371e34c05af64f7a2
Noah Greensweig <ngreensweig@gmail.com>
2024-03-12 09:41:35 -0500

    Initial styling.

---
+++ index.js
@@ -0,0 +1,17 @@
+const menu = document.querySelector('.menu');
+const nav = document.querySelector('.nav');
+const close = document.querySelector('.close');
+const body = document.querySelector('body');
+
+menu.addEventListener('click', () => {
+    nav.classList.add('nav--visible');
+    menu.classList.add('menu--hide');
+    close.classList.add('close--show');
+    body.classList.add('body--no--scroll');
+});
+close.addEventListener('click', () => {
+    nav.classList.remove('nav--visible');
+    menu.classList.remove('menu--hide');
+    close.classList.remove('close--show');
+    body.classList.remove('body--no--scroll');
+});
